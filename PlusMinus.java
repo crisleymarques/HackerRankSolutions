7c06acc0db64c5e37b318246426b3ed63bca0a8c
Crisley Marques <crisley.marques@ccc.ufcg.edu.br>
2023-04-28 12:27:09 -0300

    Add PlusMinus

--- PlusMinus.java
+++ PlusMinus.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlusMinus {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> teste = new ArrayList<>();
+        teste.add(-1);
+        teste.add(-1);
+        teste.add(0);
+        teste.add(1);
+        teste.add(1);
+        plusMinus(teste);
+    }
+
+    public static void plusMinus(List<Integer> arr) {
+        double positive = 0;
+        double negative = 0;
+        double zeros = 0;
+        for (Integer i : arr) {
+            if (i == 0) {
+                zeros++;
+            } else if (i > 0) {
+                positive++;
+            } else {
+                negative++;
+            }
+        }
+        System.out.printf("%.6f%n", positive / arr.size());
+        System.out.printf("%.6f%n", negative / arr.size());
+        System.out.printf("%.6f%n", zeros / arr.size());
+    }
 }
